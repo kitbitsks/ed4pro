1227959b1c5d951fd3ef84ab69e1b3bf8edf5692
souravkitkat2706 <souravsirvastava027@gmail.com>
2018-03-13 19:29:15 +0530

    changes to alerting

--- alerting.js
+++ alerting.js
@@ -1,3 +1,2 @@
 alert("The page gives the details of English Premier league by implementing Angular and retrieving JSON data");
-alert("Manchester United VS Tottenham Hotspur");
 alert("Click on \n 1. ->Team Name to get details of team. \n 2. -> \"VS\" to get details of match");
